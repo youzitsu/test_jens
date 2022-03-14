435053d8f8eb5c49a5a9aba0fc94a4232dc76e20
luuvanphuong13032000 <shirosoraaniki@gmail.com>
2022-03-14 15:30:11 +0700

    update file

--- a.php
+++ a.php
@@ -1,5 +1,11 @@
 <?php
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
+$x = 5; // global scope
+
+function myTest() {
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: $x</p>";
+}
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
 ?>
